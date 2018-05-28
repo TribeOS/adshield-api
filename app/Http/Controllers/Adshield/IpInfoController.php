efc1900e6eed6293a065e3f5e60e1b8ae31629ca
aman <aman@tr.be>
2018-05-28 14:36:03 +0800

    implement ip info storing

--- app/Http/Controllers/Adshield/IpInfoController.php
+++ app/Http/Controllers/Adshield/IpInfoController.php
@@ -15,9 +15,63 @@ class IpInfoController extends BaseController
 
 	public static function GetIpInfo($ip)
 	{
-		$url = 'http://ip-api.com/json/' . $ip;
-		$response = file_get_contents($url);
-		return json_decode($response, true);
+		$ipBinary = inet_pton($ip);
+		$info = DB::table("asIpCachedInfo")
+			->where("ip", $ipBinary)
+			->first();
+		if (empty($info))
+		{
+			$url = 'http://ip-api.com/json/' . $ip;
+			$response = file_get_contents($url);
+			$info = json_decode($response, true);
+			self::SaveIpInfo($ip, $info, $response);
+		}
+		else if (strtotime($info->updatedOn) < strtotime("15 days ago"))
+		{
+			$url = 'http://ip-api.com/json/' . $ip;
+			$response = file_get_contents($url);
+			$info = json_decode($response, true);
+			self::SaveIpInfo($ip, $info, $response, true);
+		}
+		else
+		{
+			$info = json_decode($info->rawInfo, true);
+		}
+
+		return $info;
+	}
+
+
+	public static function SaveIpInfo($ip, $info, $response, $expired = false)
+	{
+		//insert/replace?
+		if (!$expired)
+		{
+			DB::table("asIpCachedInfo")
+				->insert([
+					'ipStr' => $ip,
+					'ip' => inet_pton($ip),
+					'org' => $info['org'],
+					'isp' => $info['isp'],
+					'city' => $info['city'],
+					'country' => $info['country'],
+					'updatedOn' => gmdate("Y-m-d H:i:s"),
+					'rawInfo' => $response
+				]);
+		}
+		else
+		{
+			DB::table("asIpCachedInfo")
+				->update([
+					'org' => $info['org'],
+					'isp' => $info['isp'],
+					'city' => $info['city'],
+					'country' => $info['country'],
+					'updatedOn' => gmdate("Y-m-d H:i:s"),
+					'rawInfo' => $response
+				])
+				->where('ip', inet_pton($ip));
+		}
 	}
 	
 }
