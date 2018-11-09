e8f2dfbf4be7403fb7206d1377880c64766fb9be
aman <secondrnd@gmail.com>
2018-11-09 17:02:28 +0800

    fix bug on pages per minute check

--- app/Http/Controllers/Adshield/Violations/ViolationPagesPerMinuteController.php
+++ app/Http/Controllers/Adshield/Violations/ViolationPagesPerMinuteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Adshield\Violations;
 
 use App\Http\Controllers\Adshield\Violations\ViolationController;
 use DB;
+use App\Model\ViolationIp;
 
 /**
  * check the user agent for any known aggregator user agents
@@ -36,20 +37,19 @@ class ViolationPagesPerMinuteController extends ViolationController {
 	 */
 	private static function hasExceed($userKey, $ip, $data, $max)
 	{
+		date_default_timezone_set("UTC");
+		$ipId = ViolationIp::where("ip", $ip)->first();
+		$lastDate = DB::select("
+			SELECT createdOn FROM trViolationLog
+			WHERE userKey = ? AND ip = ?
+			ORDER BY createdOn DESC
+			LIMIT 2, 1", [$userKey, $ipId->id]);
 
-
-		//check if ip and userkey last log was more than 1 minute ago, remove all logs
-		$lastWasPast = DB::table("trViolationLog")
-			->join("trViolationIps", function($join) use($ip, $userKey) {
-				$join->on("trViolationIps.id", "=", "trViolationLog.ip")
-					->where("trViolationIps.ip", "=", $ip)
-					->where("trViolationLog.userKey", "=", $userKey);
-			})
-			->selectRaw("
-				IF(MAX(createdOn) < DATE_SUB(NOW(), INTERVAL 1 MINUTE), 1, 0) AS oldLogs
-			")
-			->first();
-		if ($lastWasPast->oldLogs == 1) self::removeLogs($ip, $userKey);
+		if (!empty($lastDate)) 
+		{
+			$lastDate = $lastDate[0]->createdOn;
+			if (strtotime($lastDate) < strtotime("1 minute ago")) self::removeLogs($ip, $userKey, $lastDate);
+		}
 		// check against logs for the past 1 minute if records exceed for this IP on this website
 		// only consider check after the last time the user has a pagesPerMinute violation, otherwise don't filter logs
 		$logCount = DB::table("trViolationLog")
@@ -73,13 +73,15 @@ class ViolationPagesPerMinuteController extends ViolationController {
 		return false;
 	}
 
-	private static function removeLogs($ip, $userKey)
+	private static function removeLogs($ip, $userKey, $lastDate=null)
 	{
+		$where = '';
+		if ($lastDate !== null) $where = " AND trViolationLog.createdOn <= '$lastDate'";
 		DB::delete("DELETE trViolationLog.* FROM trViolationLog
 			INNER JOIN trViolationIps ON
 				trViolationLog.ip = trViolationIps.id
 				AND trViolationIps.ip = ?
-				AND trViolationlog.userKey = ?", [$ip, $userKey]);
+				AND trViolationLog.userKey = ? $where", [$ip, $userKey]);
 	}
 
 }
