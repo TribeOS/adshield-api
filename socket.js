c7754d9c665567d2fff96016e47694fcd401ccbc
aman <secondrnd@gmail.com>
2018-12-13 11:01:18 +0800

    test visualiser

--- socket.js
+++ socket.js
@@ -29,7 +29,7 @@ io.on('connection', function(socket) {
 			channels[channel].on("message", function(channel, message) {
 				Object.keys(channels[channel].listeners).forEach(function(key) {
 		          	console.log("Sent on " + channel + ": " + message);
-		          	channels[channel].listeners[key].send(message);
+		          	// channels[channel].listeners[key].send(message);
 		        });
 			});
 		}
