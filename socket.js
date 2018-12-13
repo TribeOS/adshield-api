110c0e19d4964a9d79e6cac6108d82ad32d30a4a
aman <secondrnd@gmail.com>
2018-12-13 10:36:15 +0800

    fix socket.js

--- socket.js
+++ socket.js
@@ -17,7 +17,7 @@ io.on('connection', function(socket) {
 	socket.on("subscribe", function(channel) {
 		//channel already exists in our list
 		if (channels.hasOwnProperty(channel)) {
-				
+			channels[channel].listeners[socket.id] = socket;
 		} else {
 			//channel doesn't exists yet, create a new client/handler
 			channels[channel] = redis.createClient();
@@ -35,13 +35,6 @@ io.on('connection', function(socket) {
 	
 });
 
-redis.subscribe('adshield', function(err, count) {
-});
-redis.on('message', function(channel, message) {
-    console.log('Message Recieved: ' + message);
-    message = JSON.parse(message);
-    io.emit(channel + ':' + message.event, message.data);
-});
 http.listen(3000, function(){
     console.log('Listening on Port 3000');
 });
