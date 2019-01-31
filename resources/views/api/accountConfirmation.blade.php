ff9cc833b2e1610d147bd9eaa856131b445da966
joey <joey@tr.be>
2019-01-31 12:33:19 +0800

    added continue button on account confirmation blade

--- resources/views/api/accountConfirmation.blade.php
+++ resources/views/api/accountConfirmation.blade.php
@@ -6,4 +6,5 @@
 	<p>
 		Your account has been confirmed and activated. You can now log in using your username and password.
 	</p>
+    <p><a style="background: #28a745;color: #fff;padding: 10px 15px;border-radius: 5px;" href="https://adshield.tribeos.io/">Continue</a></p>
 @endif
