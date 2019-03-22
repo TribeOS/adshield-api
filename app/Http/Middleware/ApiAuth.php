2b90eab43aba3cef92500ccd8687423049d634c7
aman <secondrnd@gmail.com>
2019-03-22 15:10:18 +0800

    update login code

--- app/Http/Middleware/ApiAuth.php
+++ app/Http/Middleware/ApiAuth.php
@@ -40,6 +40,7 @@ class ApiAuth
                 ->header('Access-Control-Allow-Headers', 'Authorization');
         }
 
+        $access->options = json_decode($access->options);
         Config::set('user', $access);
 
         return $next($request)
